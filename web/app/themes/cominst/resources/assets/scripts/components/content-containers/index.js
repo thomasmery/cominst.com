371fa9cb53318215187586d82930432bd0d829fb
Thomas Mery <thomas.mery@gmail.com>
2017-11-14 17:16:07 +0100

    Cleanup

--- web/app/themes/cominst/resources/assets/scripts/components/content-containers/index.js
+++ web/app/themes/cominst/resources/assets/scripts/components/content-containers/index.js
@@ -1,7 +1,6 @@
 export { default as ContentContainerHome } from './ContentContainerHome';
 export { default as ContentContainerPagesAndSidebarNavigation } from './ContentContainerPagesAndSidebarNavigation';
 export { default as ContentContainerPagesAndSidebarLinks} from './ContentContainerPagesAndSidebarLinks';
-export { default as ContentContainer03} from './ContentContainer03';
 export { default as ContentContainerCarousel} from './ContentContainerCarousel';
 export { default as ContentContainerArchive } from './ContentContainerArchive';
 export { default as ContentContainerListAndModal } from './ContentContainerListAndModal';
