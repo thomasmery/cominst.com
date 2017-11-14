3e6f12c0a3b63c5b4711f9ceb01b9a2d6cfe948c
Thomas Mery <thomas.mery@gmail.com>
2017-11-14 11:37:01 +0100

    Style

--- web/app/themes/cominst/resources/assets/scripts/components/content-containers/ContentContainerPagesAndSidebarNavigation.js
+++ web/app/themes/cominst/resources/assets/scripts/components/content-containers/ContentContainerPagesAndSidebarNavigation.js
@@ -165,20 +165,18 @@ class ContentContainerPagesAndSidebarNavigation extends Component {
                           data.children[this.state.activeChildIndex].content_parts.length > 1 ?
                             <a onClick={this._onContentToggleHandler}>
                               {
-                                this.state.childContentExpanded
-                                ?
-                                <span>
-                                  <svg width="29px" height="29px" viewBox="0 0 29 29" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                                <span className={ ! this.state.childContentExpanded && 'collapsed'}>
+                                  <svg className="expand-button" width="29px" height="29px" viewBox="0 0 29 29" version="1.1" xmlns="http://www.w3.org/2000/svg">
                                       <defs></defs>
-                                      <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" strokeLinecap="square">
-                                          <g id="add" transform="translate(1.000000, 1.000000)" stroke="#018EC0" strokeWidth="2">
-                                              <path d="M0,13 L27,13" id="Line-2"></path>
-                                              <path d="M14,27 L14,0" id="Line-2-Copy"></path>
+                                      <g id="expand-button-container" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" strokeLinecap="square">
+                                          <g id="expand-button-group" transform="translate(1.000000, 1.000000)" stroke="#018EC0" strokeWidth="2">
+                                              <path d="M0,13 L27,13" id="horizontal-line"></path>
+                                              <path d="M14,27 L14,0" id="vertical-line"></path>
                                           </g>
                                       </g>
                                   </svg>
                                 </span>
-                                : 'Expand' }
+                              }
                             </a> : ''
                         }
                       </div>
