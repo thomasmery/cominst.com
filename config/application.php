213e4f02d900b97b2f5f9062cea6f9bb362a909b
Thomas Mery <thomas.mery@gmail.com>
2018-02-18 16:42:18 +0100

    Add WP Super Cache plugin

--- config/application.php
+++ config/application.php
@@ -46,6 +46,12 @@ define('WP_CONTENT_DIR', $webroot_dir . CONTENT_DIR);
 define('WP_CONTENT_URL', WP_HOME . CONTENT_DIR);
 
 /**
+ * Cache
+ */
+define('WP_CACHE', true);
+define('WPCACHEHOME', WP_CONTENT_DIR . '/plugins/wp-super-cache/');
+
+/**
  * DB settings
  */
 define('DB_NAME', env('DB_NAME'));
