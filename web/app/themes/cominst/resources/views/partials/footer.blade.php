a96a1533108c15462bc0ac92ba46272192c3910b
Thomas Mery <thomas.mery@gmail.com>
2018-02-25 18:23:59 +0100

    Add footer partial

--- web/app/themes/cominst/resources/views/partials/footer.blade.php
+++ web/app/themes/cominst/resources/views/partials/footer.blade.php
@@ -1,5 +1,65 @@
-<footer class="content-info">
-  <div class="container">
-    @php(dynamic_sidebar('sidebar-footer'))
+<section id="contact" class="dark">
+  <div class="container ContentContainerFooter">
+    <div class="section-content">
+      <div class="content-container content-container-footer">
+        <div class="row content">
+          <div class="col-md-8 col-left">
+            <a href="https://goo.gl/maps/GDFsAFmQEFu" target="_blank" class="map-container"></a>
+          </div>
+          <div class="col-md-4 col-right">
+            <div class="block contact-details-container">
+              <h3>{!! __('Contact details', 'cominst') !!}</h3>
+              <p>
+                <p>32, rue Notre-Dame-des-Victoires
+                  <br> 75002 Paris
+                  <br> France
+                  <br> Tél : +33 (0) 1 47 42 53 00
+                  <br> Fax : +33 (0) 1 47 42 24 11
+                  <br>
+                  <a href="mailto:secretariat@cominst.com" target="_blank" rel="noopener">secretariat@cominst.com</a>
+                </p>
+                <p>
+                  <a class="link-icon link-email" href="mailto:secretariat@cominst.com" target="_blank" rel="noopener">{!! __('Contact us', 'cominst') !!}</a>
+                </p>
+              </p>
+            </div>
+            <div class="block map-container">https://goo.gl/maps/GDFsAFmQEFu</div>
+            <div class="block social-networks-container">
+              <h3>{!! __('Follow Us', 'cominst') !!}</h3>
+              <ul class="social-networks">
+                <li class="item">
+                  <a href="https://www.linkedin.com/company/communication-&amp;-institutions/" target="_blank">
+                    <i class="icon fa fa-linkedin-square" aria-hidden="true"></i>
+                  </a>
+                </li>
+                <li class="item">
+                  <a href="https://twitter.com/com_inst" target="_blank">
+                    <i class="icon fa fa-twitter-square" aria-hidden="true"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <!-- <div class="block block-newsletters-container">
+                <h3>Newsletter</h3>
+                <div class="form-container">
+                    <form action="https://thomas-mery.us1.list-manage.com/subscribe/post?u=e9dcc6f157d6a768e8935280c&amp;id=3189b32b9e"
+                        method="post" novalidate="">
+                        <input type="email" name="EMAIL" required="" placeholder="Votre email" value="">
+                        <input type="submit" value="Envoyer">
+                        <div class="message"></div>
+                    </form>
+                </div>
+            </div> -->
+          </div>
+        </div>
+        <div class="row signature">
+          <div class="col-sm-12">
+            <span class="site-name">© Communications &amp; Institutions</span>
+            <span class="separator">-</span>
+            <span class="site-description">{!! get_bloginfo('description', 'display') !!}</span>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-</footer>
+</section>
--- web/app/themes/cominst/resources/views/single.blade.php
+++ web/app/themes/cominst/resources/views/single.blade.php
@@ -2,7 +2,7 @@
 @section('content') 
   @include('partials.header') 
   @while(have_posts()) @php(the_post())
-    <section id="{{ $post_slug }}" class="ContentContainerArchive">
+    <section id="the-post" class="ContentContainerArchive">
       <div class="container ContentContainerArchive">
         <div class="section-content">
           <div class="content-container content-container-archive">
@@ -39,7 +39,7 @@
                           <span>Les Echos - tribune</span>
                         </span>
                       </div>
-                      <h1 class="title">@php(the_content())</h1>
+                      <h1 class="title">@php(the_title())</h1>
                     </div>
                     <div class="content">
                       <div>
@@ -55,70 +55,6 @@
       </div>
       <div class="scroll-hint"></div>
     </section>
-    <section id="contact" class="dark">
-      <div class="container ContentContainerFooter">
-        <div class="section-content">
-          <div class="content-container content-container-footer">
-            <div class="row content">
-              <div class="col-md-8 col-left">
-                <a href="https://goo.gl/maps/GDFsAFmQEFu" target="_blank" class="map-container"></a>
-              </div>
-              <div class="col-md-4 col-right">
-                <div class="block contact-details-container">
-                  <h3>Nos coordonnées</h3>
-                  <p>
-                    <p>32, rue Notre-Dame-des-Victoires
-                      <br> 75002 Paris
-                      <br> France
-                      <br> Tél : +33 (0) 1 47 42 53 00
-                      <br> Fax : +33 (0) 1 47 42 24 11
-                      <br>
-                      <a href="mailto:secretariat@cominst.com" target="_blank" rel="noopener">secretariat@cominst.com</a>
-                    </p>
-                    <p>
-                      <a class="link-icon link-email" href="mailto:secretariat@cominst.com" target="_blank" rel="noopener">Contactez-nous</a>
-                    </p>
-                  </p>
-                </div>
-                <div class="block map-container">https://goo.gl/maps/GDFsAFmQEFu</div>
-                <div class="block social-networks-container">
-                  <h3>Nous suivre</h3>
-                  <ul class="social-networks">
-                    <li class="item">
-                      <a href="https://www.linkedin.com/company/communication-&amp;-institutions/" target="_blank">
-                        <i class="icon fa fa-linkedin-square" aria-hidden="true"></i>
-                      </a>
-                    </li>
-                    <li class="item">
-                      <a href="https://twitter.com/com_inst" target="_blank">
-                        <i class="icon fa fa-twitter-square" aria-hidden="true"></i>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-                <!-- <div class="block block-newsletters-container">
-                    <h3>Newsletter</h3>
-                    <div class="form-container">
-                        <form action="https://thomas-mery.us1.list-manage.com/subscribe/post?u=e9dcc6f157d6a768e8935280c&amp;id=3189b32b9e"
-                            method="post" novalidate="">
-                            <input type="email" name="EMAIL" required="" placeholder="Votre email" value="">
-                            <input type="submit" value="Envoyer">
-                            <div class="message"></div>
-                        </form>
-                    </div>
-                </div> -->
-              </div>
-            </div>
-            <div class="row signature">
-              <div class="col-sm-12">
-                <span class="site-name">© Communications &amp; Institutions</span>
-                <span class="separator">-</span>
-                <span class="site-description">Le lobbying expert depuis 1983</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
   @endwhile
+  @include('partials.footer')
 @endsection
