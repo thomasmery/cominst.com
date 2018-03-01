b4f54e308336a1ae08726c7d779bb4f895e21bc6
Thomas Mery <thomas.mery@gmail.com>
2018-03-01 10:35:15 +0100

    Update 404 page

--- web/app/themes/cominst/resources/views/404.blade.php
+++ web/app/themes/cominst/resources/views/404.blade.php
@@ -1,13 +1,20 @@
 @extends('layouts.app')
-
-@section('content')
-  @include('partials.page-header')
+@section('content') 
+  @include('partials.header') 
 
   @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
-    </div>
-    {!! get_search_form(false) !!}
+    <section id="the-post" class="ContentContainerArchive">
+      <div class="container ContentContainerArchive">
+        <div class="section-content">
+          <div class="content-container">
+            <div class="alert alert-warning">
+              <p>{{ __('Sorry, but the page you were trying to view does not exist.', 'cominst') }}</p>
+              <p>{{ __('Please use the menu above to navigate to any section of our site', 'cominst') }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   @endif
 
   {!! get_the_posts_navigation() !!}
