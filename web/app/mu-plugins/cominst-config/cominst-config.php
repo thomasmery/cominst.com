f1adf23b5b4a20c3a5151e9233420579a1941603
Thomas Mery <thomas.mery@gmail.com>
2018-03-28 08:18:06 +0200

    Update Theme config
    - comment
    - disable theme v1 taxonomies

--- web/app/mu-plugins/cominst-config/cominst-config.php
+++ web/app/mu-plugins/cominst-config/cominst-config.php
@@ -3,11 +3,16 @@
 Plugin Name: Communication & Institutions - Configuration plugin
 */
 
+/** 
+ * Note: in C&I v2 we don't need additional CPTs or Taxonomies
+ * we keep the config here and comment the includes for future reference
+ */
+
 // post types
 // require ('includes/post-types.php');
 
 // taxonomies
-require ('includes/taxonomies.php');
+// require ('includes/taxonomies.php');
 
 
 // WP custom fields - meta boxes
