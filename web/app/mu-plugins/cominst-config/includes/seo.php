94405fadb3f950a8ae60af348d4266d9639c9db3
Thomas Mery <thomas.mery@gmail.com>
2019-03-10 21:07:13 +0100

    Update SEO structured data for Knowledge Graph

--- web/app/mu-plugins/cominst-config/includes/seo.php
+++ web/app/mu-plugins/cominst-config/includes/seo.php
@@ -19,17 +19,46 @@ function cominst_init_seo() {
 
                     // necessary informations should be setup in the Theme Options page - General Settings
                     // this option page is setup in includes/acf/global-settings.php
-                    $address = get_field('address', 'theme-general-settings');
+
                     $phone_number = get_field('phone_number', 'theme-general-settings');
                     $contact_email = get_field('contact_email', 'theme-general-settings');
+                    
+                    $data['@type'] = 'Corporation';
+                    
+                    $data["description"] = 'Cabinet de conseil en affaires publiques et lobbying à Paris';
+                    $data['foundingDate'] = '1987-02-26';
+                    $data['image'] = $data['logo'];
+
+                    // all other infos are contributed via the admimn (social networks)
+                    array_push(
+                        $data['sameAs'],
+                        home_url('', null)
+                    );
+                    array_push(
+                        $data['sameAs'],
+                        'https://www.societe.com/societe/communication-institutions-340388479.html'
+                    );
+                    
+                    // backend contributed address was used when we had a global adress info field
+                    // for now replaced by hard coded structured data for 'address' schema.org field
+                    // because we don't want to setup the individual address fields in the admin
+                    // and can't rely on a regexp to parse the free form address 
+                    // $address = get_field('address', 'theme-general-settings');
+                    $data['address'] = [
+                        '@type' => 'PostalAddress',
+                        'addressLocality' => 'Paris',
+                        'postalCode' => '75002',
+                        'streetAddress' => '32, rue Notre-Dame-des-Victoires',
+                        'telephone' => $phone_number,
+                    ];
 
                     $data['ContactPoint'] = [
                         [
                             "@type" => "ContactPoint",
-                            "address" => $address,
                             "telephone" => $phone_number,
                             "email" => $contact_email,
-                            "contactType" => "Secrétariat / Contact",
+                            "contactType" => "customer service",
+                            "areaServed" => "France",
                         ]
                     ];
                 }
