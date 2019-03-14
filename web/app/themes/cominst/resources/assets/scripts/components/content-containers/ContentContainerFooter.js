c87c7cc16399ae5d04d46eb3e1ac4f83e46222ee
Thomas Mery <thomas.mery@gmail.com>
2019-03-14 13:35:42 +0100

    WIP contact form
    # Conflicts:
    #	web/app/themes/cominst/yarn.lock

--- web/app/themes/cominst/resources/assets/scripts/components/content-containers/ContentContainerFooter.js
+++ web/app/themes/cominst/resources/assets/scripts/components/content-containers/ContentContainerFooter.js
@@ -1,17 +1,19 @@
 /* global appData */
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import MailchimpSubscribeForm from '../tools/MailchimpSubscribeForm';
+import ContactForm from "../ContactForm";
 
-const ContentContainerFooter = function( { data }) {
+import MailchimpSubscribeForm from "../tools/MailchimpSubscribeForm";
 
+const ContentContainerFooter = function({ data }) {
   try {
-    if ( ! data.mailchimp_subscribe_url ) {
-      throw new Error('You must define a mailchimp_subscribe_url for your site');
+    if (!data.mailchimp_subscribe_url) {
+      throw new Error(
+        "You must define a mailchimp_subscribe_url for your site"
+      );
     }
-  }
-  catch (error) {
+  } catch (error) {
     console.warn(error.message); // eslint-disable-line no-console
   }
 
@@ -19,82 +21,91 @@ const ContentContainerFooter = function( { data }) {
     <div className="content-container content-container-footer">
       <div className="row content">
         <div className="col-md-8 col-left">
-          <a href={ data.map || '#' } target="_blank" rel="noopener noreferrer" className="map-container" />
+          <a
+            href={data.map || "#"}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="map-container"
+          />
           {/* <div className="map-container" dangerouslySetInnerHTML={ { __html: data.map } } /> */}
         </div>
         <div className="col-md-4 col-right">
-
           <div className="block contact-details-container">
             <h3>{appData.i18n.contact_details}</h3>
-            <p dangerouslySetInnerHTML={ { __html: data.contact_details } }/>
+            <p dangerouslySetInnerHTML={{ __html: data.contact_details }} />
+            <ContactForm />
           </div>
 
           <div className="block">
-            <a href={ data.map || '#' } target="_blank" rel="noopener noreferrer" className="map-container" />
+            <a
+              href={data.map || "#"}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="map-container"
+            />
           </div>
 
           <div className="block social-networks-container">
             <h3>{appData.i18n.follow_us}</h3>
             <ul className="social-networks">
-            {
-              data.social_networks.map(
-                (network) => (
-                  <li className="item" key={network.name}>
-                    <a href={network.url} target="_blank"rel="noopener noreferrer">
-                      <i className={`icon fa fa-${network.name.toLowerCase()}-square`} aria-hidden="true"></i>
-                    </a>
-                  </li>
-                )
-              )
-            }
+              {data.social_networks.map(network => (
+                <li className="item" key={network.name}>
+                  <a
+                    href={network.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i
+                      className={`icon fa fa-${network.name.toLowerCase()}-square`}
+                      aria-hidden="true"
+                    />
+                  </a>
+                </li>
+              ))}
             </ul>
           </div>
 
           <div className="block block-newsletters-container">
             <h3>{appData.i18n.newsletter}</h3>
-              <MailchimpSubscribeForm
-                className="form-container"
-                action={data.mailchimp_subscribe_url}
-                messages = {{
-                  inputPlaceholder: appData.i18n.newsletterSubscribeForm.inputPlaceholder,
-                  btnLabel: appData.i18n.newsletterSubscribeForm.btnLabel,
-                  sending: appData.i18n.newsletterSubscribeForm.sending,
-                  success: appData.i18n.newsletterSubscribeForm.success,
-                  error: appData.i18n.newsletterSubscribeForm.error,
-                }}
-              />
+            <MailchimpSubscribeForm
+              className="form-container"
+              action={data.mailchimp_subscribe_url}
+              messages={{
+                inputPlaceholder:
+                  appData.i18n.newsletterSubscribeForm.inputPlaceholder,
+                btnLabel: appData.i18n.newsletterSubscribeForm.btnLabel,
+                sending: appData.i18n.newsletterSubscribeForm.sending,
+                success: appData.i18n.newsletterSubscribeForm.success,
+                error: appData.i18n.newsletterSubscribeForm.error,
+              }}
+            />
           </div>
 
           <div className="block legal-infos-container">
             <h3>{data.secondary_navigation.title}</h3>
             <ul className="legal-infos-links">
-            {
-              data.secondary_navigation.items.map(
-                (item) => (
-                  <li className="item" key={item.title}>
-                    <a href={item.url}>
-                      {item.title}
-                    </a>
-                  </li>
-                )
-              )
-            }
+              {data.secondary_navigation.items.map(item => (
+                <li className="item" key={item.title}>
+                  <a href={item.url}>{item.title}</a>
+                </li>
+              ))}
             </ul>
           </div>
-
         </div>
       </div>
       <div className="row signature">
         <div className="col-sm-12">
-          <span className="site-name">© { data.site_name }</span> <span className="separator">-</span> <span className="site-description">{ data.site_description }</span>
+          <span className="site-name">© {data.site_name}</span>{" "}
+          <span className="separator">-</span>{" "}
+          <span className="site-description">{data.site_description}</span>
         </div>
       </div>
     </div>
-    );
-}
+  );
+};
 
 ContentContainerFooter.propTypes = {
   data: PropTypes.object,
-}
+};
 
 export default ContentContainerFooter;
