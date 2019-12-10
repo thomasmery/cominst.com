fa8b9fc384ab8fdf10a2beec6c32ad6e43af9dcb
Thomas Mery <thomas.mery@gmail.com>
2019-12-10 11:26:39 +0100

    Update wp-super-cache - remove from repo & add to composer
    trying to finally fix https://github.com/aaltomeri/cominst.com/issues/6
    
    note: production server has been updated to php 7.3

--- web/app/themes/cominst/app/Api.php
+++ web/app/themes/cominst/app/Api.php
@@ -5,7 +5,7 @@ namespace App;
 /**
  * we need to add some custom data to some WP REST API responses
  */
-add_action('rest_api_init', function() {
+add_action('rest_api_init', function () {
     // an image tag for featured images
     register_rest_field(
         ['post', 'page'],
@@ -59,42 +59,47 @@ add_action('rest_api_init', function() {
             'get_callback' => '\App\get_path'
         ]
     );
-
 });
 
-function get_featured_media_html($object) {
+function get_featured_media_html($object)
+{
     $html = '';
-    if(isset($object['featured_media']) && $object['featured_media']) {
-        $html = wp_get_attachment_image( $object['featured_media'], 'sm');
+    if (isset($object['featured_media']) && $object['featured_media']) {
+        $html = wp_get_attachment_image($object['featured_media'], 'sm');
     }
     return $html;
 }
 
-function get_featured_media_metadata($object) {
+function get_featured_media_metadata($object)
+{
     $metadata = [];
-    if(isset($object['featured_media']) && $object['featured_media']) {
-        $metadata  = wp_get_attachment_metadata( $object['featured_media'] );
-        foreach($metadata['sizes'] as $size => $size_infos ) {
-            $metadata['sizes'][$size]['url'] = wp_get_attachment_image_src( $object['featured_media'], $size )[0];
+    if (isset($object['featured_media']) && $object['featured_media']) {
+        $metadata  = wp_get_attachment_metadata($object['featured_media']);
+        foreach ($metadata['sizes'] as $size => $size_infos) {
+            $metadata['sizes'][$size]['url'] = wp_get_attachment_image_src($object['featured_media'], $size)[0];
         }
-        $metadata['sizes']['original']['url'] = wp_get_attachment_image_src( $object['featured_media'], null)[0];
+        $metadata['sizes']['original']['url'] = wp_get_attachment_image_src($object['featured_media'], null)[0];
     }
-    return $metadata ;
+    return $metadata;
 }
 
-function get_categories_names($object) {
+function get_categories_names($object)
+{
     return wp_get_object_terms($object['id'], 'category', ['fields' => 'names']);
 }
 
-function get_medias_names($object) {
+function get_medias_names($object)
+{
     return wp_get_object_terms($object['id'], 'media', ['fields' => 'names']);
 }
 
-function get_formatted_published_date($object) {
+function get_formatted_published_date($object)
+{
     return get_the_date(null, $object['id']);
 }
 
-function get_path($object) {
+function get_path($object)
+{
     return str_replace(get_option('home'), '', get_the_permalink($object['id']));
 }
 
@@ -106,7 +111,7 @@ function get_path($object) {
  */
 add_filter(
     'rest_category_collection_params',
-    function($params) {
+    function ($params) {
         $params['orderby']['enum'][] = 'term_order';
         return $params;
     }
@@ -120,20 +125,22 @@ add_filter(
  * (when we don't need to fetch data fro the client - and don't want to create custom endpoints)
  * with regular WP methods
  */
-class Api {
+class Api
+{
     /**
-    * get data from REST API
-    * to be used in initial data payload for JS app
-    */
+     * get data from REST API
+     * to be used in initial data payload for JS app
+     */
 
     /**
-    * posts
-    */
-    public static function get_posts( $per_page = 10, $category_id = 0 ) {
-        $request = new \WP_REST_Request( 'GET', '/wp/v2/posts' );
-        $request->set_param( 'per_page', $per_page );
-        $request->set_param( 'categories', $category_id ? [$category_id] : []);
-        $response = rest_do_request( $request );
+     * posts
+     */
+    public static function get_posts($per_page = 10, $category_id = 0)
+    {
+        $request = new \WP_REST_Request('GET', '/wp/v2/posts');
+        $request->set_param('per_page', $per_page);
+        $request->set_param('categories', $category_id ? [$category_id] : []);
+        $response = rest_do_request($request);
         return [
             'data' => $response->data,
             'category_id' => $category_id,
@@ -146,7 +153,8 @@ class Api {
     }
 
     /** get ALL Posts IDs & slug */
-    public static function get_all_posts_ids_and_slugs( ) {
+    public static function get_all_posts_ids_and_slugs()
+    {
         global $wpdb;
 
         $wpml_is_installed = defined('ICL_LANGUAGE_CODE');
@@ -160,7 +168,8 @@ class Api {
         $where_statement .= $wpml_is_installed
             ? " AND wpml.`language_code` = '$lang'"
             : '';
-        $query = sprintf("SELECT ID, post_name
+        $query = sprintf(
+            "SELECT ID, post_name
             FROM wp_posts p
             %s
             WHERE %s
@@ -173,49 +182,54 @@ class Api {
     }
 
     /**
-    * post categories
-    */
-    public static function get_categories( $per_page = 99 ) {
-        $request = new \WP_REST_Request( 'GET', '/wp/v2/categories' );
-        $request->set_param( 'per_page', $per_page );
-        $request->set_param( 'orderby', 'term_order' );
-        $response = rest_do_request( $request );
+     * post categories
+     */
+    public static function get_categories($per_page = 99)
+    {
+        $request = new \WP_REST_Request('GET', '/wp/v2/categories');
+        $request->set_param('per_page', $per_page);
+        $request->set_param('orderby', 'term_order');
+        $response = rest_do_request($request);
         return $response->data;
     }
 
     /**
-    * taxonomies
-    */
-    public static function get_taxonomies( $per_page = 99 ) {
-        $request = new \WP_REST_Request( 'GET', '/wp/v2/taxonomies' );
-        $request->set_param( 'per_page', $per_page );
-        $response = rest_do_request( $request );
+     * taxonomies
+     */
+    public static function get_taxonomies($per_page = 99)
+    {
+        $request = new \WP_REST_Request('GET', '/wp/v2/taxonomies');
+        $request->set_param('per_page', $per_page);
+        $response = rest_do_request($request);
         return $response->data;
     }
 
     /**
-    * post types
-    */
-    public static function get_post_types( $per_page = 99 ) {
-        $request = new \WP_REST_Request( 'GET', '/wp/v2/types' );
-        $request->set_param( 'per_page', $per_page );
-        $response = rest_do_request( $request );
+     * post types
+     */
+    public static function get_post_types($per_page = 99)
+    {
+        $request = new \WP_REST_Request('GET', '/wp/v2/types');
+        $request->set_param('per_page', $per_page);
+        $response = rest_do_request($request);
         return $response->data;
     }
 
     /**
-    * pages
-    */
-    public static function get_pages( $params ) {
-        $request = new \WP_REST_Request( 'GET', '/wp/v2/pages' );
-        foreach($params as $key => $value) {
-            $request->set_param( $key, $value );
+     * pages
+     */
+    public static function get_pages($params)
+    {
+        $request = new \WP_REST_Request('GET', '/wp/v2/pages');
+        foreach ($params as $key => $value) {
+            $request->set_param($key, $value);
         }
-        $response = rest_do_request( $request );
+        $response = rest_do_request($request);
         return $response->data;
     }
 
-    public static function get_top_pages() {
+    public static function get_top_pages()
+    {
         $params = [
             'per_page' => 99, // max allowed is 99 - if more is needed > make anothe call and combine
             // 'parent' => 0
@@ -224,15 +238,16 @@ class Api {
     }
 
     /**
-    * menus
-    */
+     * menus
+     */
 
     /**
      * Main navigation
      */
-    public static function get_primary_navigation() {
-        $request = new \WP_REST_Request( 'GET', '/wp-api-menus/v2/menu-locations/primary_navigation');
-        $response = rest_do_request( $request );
+    public static function get_primary_navigation()
+    {
+        $request = new \WP_REST_Request('GET', '/wp-api-menus/v2/menu-locations/primary_navigation');
+        $response = rest_do_request($request);
         return $response->data;
     }
 
@@ -241,10 +256,11 @@ class Api {
      *
      * we treat this one differently as we need a title to be displayed before the items
      */
-    public static function get_secondary_navigation() {
+    public static function get_secondary_navigation()
+    {
         $location = 'secondary_navigation';
         $locations  = get_nav_menu_locations();
-        if ( ! isset( $locations[$location] ) ) {
+        if (!isset($locations[$location])) {
             return null;
         }
         $menu_id = $locations[$location];
@@ -252,8 +268,8 @@ class Api {
         $title = $wp_menu->name;
 
         // use the endpoint to benefit from formatting done by the wp-api-menu plugin
-        $request = new \WP_REST_Request( 'GET', '/wp-api-menus/v2/menu-locations/secondary_navigation');
-        $response = rest_do_request( $request );
+        $request = new \WP_REST_Request('GET', '/wp-api-menus/v2/menu-locations/secondary_navigation');
+        $response = rest_do_request($request);
         $items = $response->data;
 
         $navigation = [
@@ -268,9 +284,10 @@ class Api {
      * Theme options
      * used for Client infos like contact details, social networks ...
      */
-    public static function get_theme_options() {
-        $request = new \WP_REST_Request( 'GET', '/acf/v3/options/theme-general-settings' );
-        $response = rest_do_request( $request );
+    public static function get_theme_options()
+    {
+        $request = new \WP_REST_Request('GET', '/acf/v3/options/theme-general-settings');
+        $response = rest_do_request($request);
         return $response->data;
     }
 
@@ -280,30 +297,36 @@ class Api {
      * we are not using the REST API because it would require custom endpoints
      * and we won't need to fetch the date from the client
      */
-    public static function get_references_by_sectors() {
+    public static function get_references_by_sectors()
+    {
         $sectors = get_terms(['taxonomy' => 'sector', 'orderby' => 'name']);
-        foreach($sectors as $key => $sector) {
+        foreach ($sectors as $key => $sector) {
             $sectors[$key]->image = get_field('image', $sector);
-            if(isset($sectors[$key]->image) && $sectors[$key]->image) {
-                $sectors[$key]->image_html = wp_get_attachment_image( $sectors[$key]->image['ID'], 'medium');
+            if (isset($sectors[$key]->image) && $sectors[$key]->image) {
+                $sectors[$key]->image_html = wp_get_attachment_image($sectors[$key]->image['ID'], 'medium');
             }
             $sectors[$key]->references = get_posts([
                 'post_type' => 'reference',
-                'tax_query' => [ ['taxonomy' => 'sector', 'field' => 'term_id', 'terms' => [$sector->term_id] ] ]
+                'tax_query' => [['taxonomy' => 'sector', 'field' => 'term_id', 'terms' => [$sector->term_id]]]
             ]);
         }
         return $sectors;
     }
-
 }
 
-remove_action( 'template_redirect', 'wp_shortlink_header', 11);
+/**
+ * comment this de-activation out
+ * because we're attempting to fix https://github.com/aaltomeri/cominst.com/issues/6
+ * by updating wp-super-cache from the source
+ */
+// remove_action( 'template_redirect', 'wp_shortlink_header', 11);
 
 /** Contact Form - response function  */
-function cominst_contact_form() {
+function cominst_contact_form()
+{
 
     // will return a 403 error if nonce can't be verified
-    check_ajax_referer( 'contactForm', 'nonce' );
+    check_ajax_referer('contactForm', 'nonce');
 
     $data = $_POST['formData'];
 
@@ -325,10 +348,9 @@ function cominst_contact_form() {
 
     $mail = wp_mail($to, $subject, $message, $headers);
 
-    if($mail){
+    if ($mail) {
         echo 'success';
-    }
-    else {
+    } else {
         echo __('Error sending email', 'cominst');
     }
     wp_die();
